513777e99f284d3bde76fb4624861234ab92ec42
SHYNGGYSKHAN <98832316+QANTBEK@users.noreply.github.com>
2022-10-10 21:52:31 +0600

    Add files via upload

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -15,8 +15,8 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 }
 func (r *AuthPostgres) CreateUser(user goapp.User) (int, error) {
 	var id int
-	query := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
-	row := r.db.QueryRow(query, user.Name, user.Password)
+	query := fmt.Sprintf("INSERT INTO: %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
+	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
